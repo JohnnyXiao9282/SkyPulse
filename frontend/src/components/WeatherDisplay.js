86f172fed02d2e36c35498828bade0c1efe44c5c
Johnny Xiao <yuxiao9282@gmail.com>
2026-03-18 18:47:34 -0700

    update

--- frontend/src/components/WeatherDisplay.js
+++ frontend/src/components/WeatherDisplay.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React from "react";
 
 function WeatherDisplay({ weather }) {
-  if (!weather || !weather.weather || !Array.isArray(weather.weather) || !weather.weather[0]) {
+  if (
+    !weather ||
+    !weather.weather ||
+    !Array.isArray(weather.weather) ||
+    !weather.weather[0]
+  ) {
     return <div className="weather-display">No weather data available.</div>;
   }
   return (
     <div className="weather-display">
       <h2>Current Weather</h2>
       <div>
-        <strong>{weather.weather[0].main}</strong> - {weather.weather[0].description}
+        <strong>{weather.weather[0].main}</strong> -{" "}
+        {weather.weather[0].description}
       </div>
       <div>Temperature: {weather.main.temp}°C</div>
       <div>Humidity: {weather.main.humidity}%</div>
       <div>Wind: {weather.wind.speed} m/s</div>
-      <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="icon" />
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt="icon"
+      />
     </div>
   );
 }
